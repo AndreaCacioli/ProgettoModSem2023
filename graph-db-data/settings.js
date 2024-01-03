d86fddb5c29f4f25043541ba07116036620a83e5
Cava <samuele.cavagnino@edu.unito.it>
2024-01-03 10:33:55 +0100

    docker compose e immagini

---
+++ graph-db-data/settings.js
@@ -0,0 +1,31 @@
+{
+  "properties" : {
+    "current.location" : ""
+  },
+  "import.local" : {
+    "OntoGames;;https://raw.githubusercontent.com/AndreaCacioli/ProgettoModSem2023/main/OntoGames.rdf" : {
+      "name" : "https://raw.githubusercontent.com/AndreaCacioli/ProgettoModSem2023/main/OntoGames.rdf",
+      "status" : "DONE",
+      "message" : "Imported successfully in 1s.",
+      "context" : "",
+      "replaceGraphs" : [ ],
+      "baseURI" : null,
+      "forceSerial" : false,
+      "type" : "url",
+      "format" : "",
+      "data" : "https://raw.githubusercontent.com/AndreaCacioli/ProgettoModSem2023/main/OntoGames.rdf",
+      "timestamp" : 1704274314713,
+      "parserSettings" : {
+        "preserveBNodeIds" : false,
+        "failOnUnknownDataTypes" : false,
+        "verifyDataTypeValues" : false,
+        "normalizeDataTypeValues" : false,
+        "failOnUnknownLanguageTags" : false,
+        "verifyLanguageTags" : true,
+        "normalizeLanguageTags" : false,
+        "stopOnError" : true,
+        "contextLink" : null
+      }
+    }
+  }
+}
