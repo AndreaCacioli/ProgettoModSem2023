35cc8bb4e10fbbd0c0cadc7c9ecd276bea7c7c45
Andrea Cacioli <andrea.cacioli@edu.unito.it>
2024-01-04 16:49:14 +0100

    Started query component

--- OntoGames-frontend/src/app/components/query-editor/query-editor.component.ts
+++ OntoGames-frontend/src/app/components/query-editor/query-editor.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
-
+import { MatFormFieldControl, MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { FormControl, FormGroup, Validators, FormsModule} from '@angular/forms';
 @Component({
   selector: 'app-query-editor',
   standalone: true,
-  imports: [],
+  imports: [MatFormFieldModule, MatButtonModule, FormsModule],
   templateUrl: './query-editor.component.html',
   styleUrl: './query-editor.component.scss'
 })
 export class QueryEditorComponent {
+  public queryEditorForm = new FormGroup({
+    query: new FormControl<string>("", [Validators.required]),
+  })
+
+  protected sendQuery() {
+    console.log(this.queryEditorForm.value.query);
+  }
 
 }
