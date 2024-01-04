8d839335e0e0cc69dd78065b2d200ced747664cb
Andrea Cacioli <andrea.cacioli@edu.unito.it>
2024-01-04 16:20:09 +0100

    Initialized the frontend in Angular

---
+++ OntoGames-frontend/src/app/components/query-editor/query-editor.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-query-editor',
+  standalone: true,
+  imports: [],
+  templateUrl: './query-editor.component.html',
+  styleUrl: './query-editor.component.scss'
+})
+export class QueryEditorComponent {
+
+}
