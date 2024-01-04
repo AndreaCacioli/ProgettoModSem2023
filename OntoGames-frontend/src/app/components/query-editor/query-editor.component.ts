4d4b031060159bbbfab5a2b2cc9436ef3cd484c2
Cava <samuele.cavagnino@edu.unito.it>
2024-01-04 18:02:10 +0100

    Da gestire: Connettore, Query

--- OntoGames-frontend/src/app/components/query-editor/query-editor.component.ts
+++ OntoGames-frontend/src/app/components/query-editor/query-editor.component.ts
@@ -1,21 +1,25 @@
 import { Component } from '@angular/core';
 import { MatFormFieldControl, MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
-import { FormControl, FormGroup, Validators, FormsModule} from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+
 @Component({
   selector: 'app-query-editor',
   standalone: true,
-  imports: [MatFormFieldModule, MatButtonModule, FormsModule],
+  imports: [MatFormFieldModule, MatButtonModule, FormsModule, ReactiveFormsModule, MatInputModule],
   templateUrl: './query-editor.component.html',
   styleUrl: './query-editor.component.scss'
 })
 export class QueryEditorComponent {
   public queryEditorForm = new FormGroup({
-    query: new FormControl<string>("", [Validators.required]),
+    query: new FormControl(""),
   })
 
-  protected sendQuery() {
+
+  protected async sendQuery() {
     console.log(this.queryEditorForm.value.query);
+    
   }
 
 }
